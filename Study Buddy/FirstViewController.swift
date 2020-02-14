d52bbb9fa6a46fc884d4e93dfa4a724caedc700f
Vitor Jacinto <vitor.rjacinto@gmail.com>
2020-02-14 15:03:14 +0100

    Initial Commit

---
+++ Study Buddy/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Study Buddy
+//
+//  Created by Vitor Rodrigues on 14/02/2020.
+//  Copyright © 2020 Scrubs. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Study Buddy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Study Buddy
+//
+//  Created by Vitor Rodrigues on 14/02/2020.
+//  Copyright © 2020 Scrubs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
